b92f35de02d45c788c9e8180134d634f9d52589c
4eXa <ila88298@gmail.com>
2024-12-19 16:02:50 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,17 @@
+class User():
+    pass
+user = User()
+user.name = 'john' 
+user.surname = 'smit'
+print(user.name)
+print(user.surname)
+
+class Employee:
+    pass
+employee = Employee()
+employee.name = 'Oleg'
+employee.age = 30
+employee.salary = 5000000
+print(employee.name)
+print(employee.age)
+print(employee.salary)
